8822d1cc414224f9ee9064d660a57297ad500925
Георгий Петров <yegorf2010@gmail.com>
2016-10-23 14:23:13 +0500

    first commit

---
+++ _2_Abstract Factory/1_AF/IAutoFactory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_AF {
+    #region Interfaces
+    public interface IAutoFactory {
+        IBody CreateIBody();
+        IEngine CreateIEngine();
+        ICabin CreateICabin();
+        IChassis CreateIChassis();
+    }
+    public interface IBody {    //кузов
+        string Name { get; }
+    }
+    public interface IEngine {  //двигатель
+        string Name { get; }
+    }
+    public interface ICabin {   //салон
+        string Name { get; }
+    }
+    public interface IChassis { //шасси
+        string Name { get; }
+    }
+    #endregion
+
+    #region CarBMW
+    public class BMWFactory: IAutoFactory {
+        public IBody CreateIBody() {
+            return new Sedan();
+        }
+        public IEngine CreateIEngine() {
+            return new Diesel();
+        }
+        public ICabin CreateICabin() {
+            return new Leather();
+        }
+        public IChassis CreateIChassis() {
+            return new Adaptive();
+        }
+    }
+    public class Sedan: IBody {
+        public string Name => "Седан - закрытый кузов";
+    }
+    public class Diesel: IEngine {
+        public string Name => "Дизельный";
+    }
+    public class Leather: ICabin {
+        public string Name => "Натуральная кожа";
+    }
+    public class Adaptive: IChassis {
+        public string Name => "Адаптивная ходовая часть";
+    }
+    #endregion
+
+    #region CarAUDI
+    public class AUDIFactory: IAutoFactory {
+        public IBody CreateIBody() {
+            return new Cabriolet();
+        }
+        public IEngine CreateIEngine() {
+            return new Einspritzung();
+        }
+        public ICabin CreateICabin() {
+            return new Plastic();
+        }
+        public IChassis CreateIChassis() {
+            return new M_Type();
+        }
+    }
+    public class Cabriolet: IBody {
+        public string Name => "Кабриолет - открытый кузов";
+    }
+    public class Einspritzung: IEngine {
+        public string Name => "Инжекторный";
+    }
+    public class Plastic: ICabin {
+        public string Name => "Элитный пластик";
+    }
+    public class M_Type: IChassis {
+        public string Name => "ходовая часть М-типа";
+    }
+    #endregion
+}
---
+++ _2_Abstract Factory/1_AF/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_AF {
+    class Program {
+        public class Client1 {
+            IBody body_;
+            ICabin cabin_;
+            IChassis chassis_;
+            IEngine engine_;
+            public Client1(IAutoFactory factory_) {
+                body_=factory_.CreateIBody();
+                cabin_=factory_.CreateICabin();
+                chassis_=factory_.CreateIChassis();
+                engine_=factory_.CreateIEngine();
+            }
+            public void makePhoto() {
+                Console.WriteLine("Делаем фото на фоне кузова "+
+                           body_.Name+" и кабины "+cabin_.Name);
+            }
+            public void goRepair() {
+                Console.WriteLine("Едем чинить шасси "+
+                chassis_.Name+" и двигатель "+engine_.Name);
+            }
+        }
+        static void Main(string[] args) {
+            var audi = new AUDIFactory();
+            Conveyor(audi);
+
+            var bmw = new BMWFactory();
+            Conveyor(bmw);
+
+
+            var audi_car = new AUDIFactory();
+            var c1 = new Client1(audi_car);
+            c1.makePhoto();
+            c1.goRepair();
+
+            Console.Read();
+        }
+        public static void Conveyor(IAutoFactory factory) {
+            Console.WriteLine("=== Ковейер автомобильный ===");
+            var body = factory.CreateIBody();
+            var cabin = factory.CreateICabin();
+            var engine = factory.CreateIEngine();
+            var chassis = factory.CreateIChassis();
+
+            Console.WriteLine($"Создан кузов: {body.Name}");
+            Console.WriteLine($"Создана кабина: {cabin.Name}");
+            Console.WriteLine($"Создан двигатель: {engine.Name}");
+            Console.WriteLine($"Созданы шасси: {chassis.Name}");
+
+            Console.WriteLine();
+        }
+    }
+}
