bb7e01299f4c9a095e48661510e98181f6dc2689
akjerop <yegorf2010@gmail.com>
2016-11-18 12:59:56 +0500

    adding Decorator for Chat

---
+++ _5_Decorator of Chat/1_Decorator/IChat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Decorator {
+    public abstract class IChat {
+        public virtual string Author { get; }
+        public virtual string Recipient { get; }
+        public virtual string Text { get; }
+
+        public virtual void sendMsg() { }
+        public virtual string getMsg() { throw new NotImplementedException(); }
+    }
+    public class Facebook: IChat {
+        public override string Author => "edu@xrm.ru";
+        public override string Recipient => "ivanov@mail.ru";
+        public override string Text => getMsg();
+
+        public override void sendMsg() { Console.WriteLine(Text); }
+        public override string getMsg() { return "Завтра занятия по курсу отменяются!"; }
+    }
+    public class Vk: IChat {
+        public override string Author => "fti@urfu.ru";
+        public override string Recipient => "ivanov@mail.ru";
+        public override string Text => setMsg();
+
+        public override void sendMsg() { Console.WriteLine("Отправка сообщения вконктакте..."); }
+        public override string getMsg() { return ("!Непрочитанное сообщение!"); }
+        public string setMsg() { return "Дорогие выпускники..."; }
+    }
+}
---
+++ _5_Decorator of Chat/1_Decorator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Decorator {
+    class Program {
+        abstract class IDecorator: IChat {
+            protected IChat chat;
+            public IDecorator(IChat c) { chat=c; }
+        }
+        //согласно заданию нужен конкретный Декоратор по шифровке/расшифровке сообщения, т.е. поля Text
+        class EncrypMsg: IDecorator {
+            public EncrypMsg(IChat c) : base(c) { }
+
+            public override string Author => chat.Author;
+            public override string Recipient => chat.Recipient;
+            public override string Text => getMsg();
+
+            public override void sendMsg() { Console.WriteLine(Text); }
+            public override string getMsg() {
+                StringBuilder encrypt = new StringBuilder(chat.getMsg());
+                int hash = 7;
+                for (int i = 0; i<encrypt.Length; i++) { encrypt[i]=(char)(encrypt[i]^hash); }  //XOR взаимообратен
+
+                return encrypt.ToString();
+            }
+        }
+        //согласно заданию нужен конкретный Декоратор по сокрытию пользователей, про раскрытие не говорится
+        class SecretName: IDecorator {
+            public SecretName(IChat c) : base(c) { }
+
+            public override string Author => "Id {"+setIdName()+"}";
+            public override string Recipient => "Id {"+setIdRec()+"}";
+            public override string Text => chat.Text;
+
+            public override void sendMsg() { chat.sendMsg(); }
+            public override string getMsg() { return chat.getMsg(); }
+            public string setIdName() {
+                int id = 1001;
+                return id.ToString();
+            }
+            public string setIdRec() {
+                int id = 1212;
+                return id.ToString();
+            }
+        }
+        static void Main(string[] args) {
+            IChat chat1 = new Vk();
+            chat1=new SecretName(chat1);
+            Console.WriteLine($"Автор: {chat1.Author}\nАдресат: {chat1.Recipient}\nПисьмо: {chat1.Text}\n");
+
+            IChat chat2 = new Facebook();
+            Console.WriteLine($"До шифрования сообщения:\nАвтор: {chat2.Author}\nАдресат: {chat2.Recipient}\nПисьмо: {chat2.getMsg()}\n");
+            chat2=new EncrypMsg(chat2);
+            Console.WriteLine($"После шифрования сообщения:\nАвтор: {chat2.Author}\nАдресат: {chat2.Recipient}\nПисьмо: {chat2.getMsg()}\n");
+            chat2=new EncrypMsg(chat2);
+            Console.WriteLine($"После расшифровки сообщения:\nАвтор: {chat2.Author}\nАдресат: {chat2.Recipient}\nПисьмо: {chat2.getMsg()}\n");
+
+            //я так понимаю, что конструкция 'chat2=new EncrypMsg(chat2)' должна быть взаимообратной:
+            //1ое "оборачивание" шифрует, 2ое - расшифровывает
+
+            Console.Read();
+        }
+    }
+}
