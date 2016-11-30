41c5d4c23f5cf4ddb013a7da7ef70d96ab1f9810
akjerop <yegorf2010@gmail.com>
2016-12-01 00:06:52 +0500

    addint the Chain

---
+++ _6_Chain od Responsibility/1_/Handlers.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ {
+    #region Banknote
+    public enum CurrencyType { Eur, Dollar, Rub }
+    public abstract class IBanknote {
+        public abstract CurrencyType Currency { get; }
+        public abstract string sign { get; }
+        public abstract string value_ { get; }
+    }
+    public class Dollar: IBanknote {
+        public override CurrencyType Currency => CurrencyType.Dollar;
+        public override string sign => "$";
+        public override string value_ { get; }
+
+        public Dollar(string v) { value_=v; }
+    }
+    public class Euro: IBanknote {
+        public override CurrencyType Currency => CurrencyType.Eur;
+        public override string sign => "euro";
+        public override string value_ { get; }
+
+        public Euro(string v) { value_=v; }
+    }
+    public class Ruble: IBanknote {
+        public override CurrencyType Currency => CurrencyType.Rub;
+        public override string sign => "руб";
+        public override string value_ { get; }
+
+        public Ruble(string v) { value_=v; }
+    }
+    #endregion
+
+    public abstract class BanknoteHandler {
+        private readonly BanknoteHandler _nextHandler;
+
+        protected BanknoteHandler(BanknoteHandler nextHandler) { _nextHandler=nextHandler; }
+
+        public virtual bool Validate(IBanknote banknote) {
+            if (_nextHandler!=null&&_nextHandler.Validate(banknote)) {
+                return true;
+            }
+            else {
+                if (_nextHandler==null)
+                    Console.WriteLine();
+                return false;
+            }
+        }
+    }
+
+    #region Ruble Handlers
+    public abstract class RubleHandlerBase: BanknoteHandler {
+        public override bool Validate(IBanknote banknote) {
+            int money;
+            if (!(Int32.TryParse(banknote.value_, out money)))
+                Console.WriteLine("Введите число.");
+            int cnt = (money/Value);
+
+            if (cnt!=0) {
+                getMoney(cnt);
+                money=money-Value*cnt;
+                if (money>=10||money!=0)
+                    Console.Write(" +");
+            }
+            if (money<10&&money!=0) {
+                Console.WriteLine(" "+money.ToString()+" не валидная сумма =(");
+                return false;
+            }
+            return base.Validate(new Ruble(money.ToString()));
+        }
+        protected abstract int Value { get; }
+        protected RubleHandlerBase(BanknoteHandler nextHandler) : base(nextHandler) { }
+        public abstract void getMoney(int count);
+    }
+
+    public class FiveThousandRubleHandler: RubleHandlerBase {
+        public FiveThousandRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 5000;
+        public override void getMoney(int count) { Console.Write(" 5000"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class ThousandDRubleHandler: RubleHandlerBase {
+        public ThousandDRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 1000;
+        public override void getMoney(int count) { Console.Write(" 1000"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class FiveHundredRubleHandler: RubleHandlerBase {
+        public FiveHundredRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 500;
+        public override void getMoney(int count) { Console.Write(" 500"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class HundredRubleHandler: RubleHandlerBase {
+        public HundredRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 100;
+        public override void getMoney(int count) { Console.Write(" 100"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class FiftyRubleHandler: RubleHandlerBase {
+        public FiftyRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 50;
+        public override void getMoney(int count) { Console.Write(" 50"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TenRubleHandler: RubleHandlerBase {
+        public TenRubleHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 10;
+        public override void getMoney(int count) { Console.Write(" 10"); if (count>1) Console.Write("x"+count); }
+    }
+    #endregion
+
+    #region Dollar Handlers
+    public abstract class DollarHandlerBase: BanknoteHandler {
+        public override bool Validate(IBanknote banknote) {
+            int money;
+            if (!(Int32.TryParse(banknote.value_, out money))&&money>0)
+                Console.WriteLine("Введите число.");
+            int cnt = (money/Value);
+
+            if (cnt!=0) {
+                getMoney(cnt);
+                money=money-Value*cnt;
+                if (money!=0)
+                    Console.Write(" +");
+            }
+            return base.Validate(new Dollar(money.ToString()));
+        }
+        protected abstract int Value { get; }
+        protected DollarHandlerBase(BanknoteHandler nextHandler) : base(nextHandler) { }
+        public abstract void getMoney(int count);
+    }
+
+    public class HundredDollarHandler: DollarHandlerBase {
+        public HundredDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 100;
+        public override void getMoney(int count) { Console.Write(" 100"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class FiftyDollarHandler: DollarHandlerBase {
+        public FiftyDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 50;
+        public override void getMoney(int count) { Console.Write(" 50"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TwentyDollarHandler: DollarHandlerBase {
+        public TwentyDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 20;
+        public override void getMoney(int count) { Console.Write(" 20"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TenDollarHandler: DollarHandlerBase {
+        public TenDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 10;
+        public override void getMoney(int count) { Console.Write(" 10"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class FiveDollarHandler: DollarHandlerBase {
+        public FiveDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 5;
+        public override void getMoney(int count) { Console.Write(" 5"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TwoDollarHandler: DollarHandlerBase {
+        public TwoDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 2;
+        public override void getMoney(int count) { Console.Write(" 2"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class OneDollarHandler: DollarHandlerBase {
+        public OneDollarHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 1;
+        public override void getMoney(int count) { Console.Write(" 1"); if (count>1) Console.Write("x"+count); }
+    }
+    #endregion
+
+    #region Euro Handlers
+    public abstract class EuroHandlerBase: BanknoteHandler {
+        public override bool Validate(IBanknote banknote) {
+            int money;
+            if (!(Int32.TryParse(banknote.value_, out money)))
+                Console.WriteLine("Введите число.");
+            int cnt = (money/Value);
+
+            if (cnt!=0) {
+                getMoney(cnt);
+                money=money-Value*cnt;
+                if (money>=10||money!=0)
+                    Console.Write(" +");
+            }
+            if (money<5&&money!=0) {
+                Console.WriteLine(" "+money.ToString()+" не валидная сумма =(");
+                return false;
+            }
+            return base.Validate(new Euro(money.ToString()));
+        }
+        protected abstract int Value { get; }
+        protected EuroHandlerBase(BanknoteHandler nextHandler) : base(nextHandler) { }
+        public abstract void getMoney(int count);
+    }
+
+    public class FiveHundredEuroHandler: EuroHandlerBase {
+        public FiveHundredEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 500;
+        public override void getMoney(int count) { Console.Write(" 500"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TwoHundredEuroHandler: EuroHandlerBase {
+        public TwoHundredEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 200;
+        public override void getMoney(int count) { Console.Write(" 200"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class HundredEuroHandler: EuroHandlerBase {
+        public HundredEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 100;
+        public override void getMoney(int count) { Console.Write(" 100"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class FiftyEuroHandler: EuroHandlerBase {
+        public FiftyEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 50;
+        public override void getMoney(int count) { Console.Write(" 50"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TwentyEuroHandler: EuroHandlerBase {
+        public TwentyEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 20;
+        public override void getMoney(int count) { Console.Write(" 20"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class TenEuroHandler: EuroHandlerBase {
+        public TenEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 10;
+        public override void getMoney(int count) { Console.Write(" 10"); if (count>1) Console.Write("x"+count); }
+    }
+
+    public class FiveEuroHandler: EuroHandlerBase {
+        public FiveEuroHandler(BanknoteHandler nextHandler) : base(nextHandler) { }
+
+        protected override int Value => 5;
+        public override void getMoney(int count) { Console.Write(" 5"); if (count>1) Console.Write("x"+count); }
+    }
+    #endregion
+}
---
+++ _6_Chain od Responsibility/1_/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ {
+    class Program {
+        public class Bancomat {
+            private readonly BanknoteHandler _handler;
+
+            public Bancomat(IBanknote banknote) {
+                if (banknote is Dollar) {
+                    _handler=new OneDollarHandler(null);                // 1
+                    _handler=new TwoDollarHandler(_handler);            // 2
+                    _handler=new FiveDollarHandler(_handler);           // 5
+                    _handler=new TenDollarHandler(_handler);            // 10
+                    _handler=new TwentyDollarHandler(_handler);         // 20
+                    _handler=new FiftyDollarHandler(_handler);          // 50
+                    _handler=new HundredDollarHandler(_handler);        // 100
+                }
+                else if (banknote is Euro) {
+                    _handler=new FiveEuroHandler(null);                 // 5
+                    _handler=new TenEuroHandler(_handler);              // 10
+                    _handler=new TwentyEuroHandler(_handler);           // 20
+                    _handler=new FiftyEuroHandler(_handler);            // 50
+                    _handler=new HundredEuroHandler(_handler);          // 100
+                    _handler=new TwoHundredEuroHandler(_handler);       // 200
+                    _handler=new FiveHundredEuroHandler(_handler);      // 500
+                }
+                else if (banknote is Ruble) {
+                    _handler=new TenRubleHandler(null);                    // 10
+                    _handler=new FiftyRubleHandler(_handler);              // 50
+                    _handler=new HundredRubleHandler(_handler);            // 100
+                    _handler=new FiveHundredRubleHandler(_handler);        // 500
+                    _handler=new ThousandDRubleHandler(_handler);          // 1000
+                    _handler=new FiveThousandRubleHandler(_handler);       // 5000
+                }
+            }
+            public bool Validate(IBanknote banknote) {
+                Console.Write($"{banknote.value_} {banknote.sign} =");
+                return _handler.Validate(banknote);
+            }
+        }
+        static void Main(string[] args) {
+            var dollars = new Dollar("10");            // хотим снять доллары
+            var bankomat = new Bancomat(dollars);      // выбираем эту сумму в банкомате
+            bankomat.Validate(dollars);                //проверяем
+
+            var rubs = new Ruble("10");                 // тест: 1050, 2033, 1, 10, 11   
+            var bankomat2 = new Bancomat(rubs);
+            bankomat2.Validate(rubs);
+
+            var euros = new Euro("10");
+            var bankomat3 = new Bancomat(euros);
+            bankomat3.Validate(euros);
+
+            Console.Read();
+        }
+        /* рубли (7 купюр): 10, 50, 100, 500, 1000, 5000
+ * доллары (7 купюр): 1, 2, 5, 10, 20, 50, 100
+ * евро (7 купюр): 5, 10, 20, 50, 100, 200, 500
+ * 
+ * 1. неужели нужно столько хэндеров?
+ * 2. мб есть смысл сделать через дженерики, а не if?
+ * 3. все таки в Validate я могу передать любую валюту и это не контролируется!
+ * 4. return base.Validate(new Ruble(money.ToString()));   тут нет динамики
+ * 5. нужно ли реализовывать конечное число купюр и как их задавать изначально банкомату
+ * 6. я столько ошибок сделал пока выстраивал цепочку
+ * 7. до какого числа домашки сдавать, до 1 декабря?
+ */
+
+    }
+}
